9dd70b415675f4dce96f3b4301dd33614e590323
Sarvesh Kulkarni <kulkarnisarvesh1@gmail.com>
2023-11-24 13:19:47 +0530

    updated

--- project/main/urls.py
+++ project/main/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('home/', home, name="home"),
-    path('home/profile/', profile, name="profile"),
-    path('home/chat/', chats, name="chats"),
-    path('home/chat/<str:username>/', chatpage, name="chatpage"),
-    path('home/group/', groups, name="groups"),
-    path('home/contact/', contacts, name="contacts"),
-    path('home/setting/', settings, name="settings"),
+    # path('home/', home, name="home"),
+    # path('home/profile/', profile, name="profile"),
+    path('chat/', chats, name="chats"),
+    path('chat/<str:username>/', chatpage, name="chatpage"),
+    # path('home/group/', groups, name="groups"),
+    # path('home/contact/', contacts, name="contacts"),
+    # path('home/setting/', settings, name="settings"),
 ]
